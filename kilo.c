2e094ae4cb2ab40e79ba4b88cda4f50b99b95701
seth-beaty <seth.a.beaty@gmail.com>
2024-09-18 15:55:05 -0400

    Limit horizontal scroll (Step 77)

--- kilo.c
+++ kilo.c
@@ -298,6 +298,8 @@ void editorRefreshScreen() {
 /*** input ***/
 
 void editorMoveCursor(int key) {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
   switch (key)
   {
   case ARROW_LEFT:
@@ -306,7 +308,9 @@ void editorMoveCursor(int key) {
     }
     break;
   case ARROW_RIGHT:
-    E.cx++;
+    if (row && E.cx < row->size) {
+      E.cx++;
+    }
     break;
   case ARROW_UP:
     if (E.cy != 0){
@@ -318,6 +322,12 @@ void editorMoveCursor(int key) {
       E.cy++;
     }
     break;
+  }
+
+  row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row->size : 0;
+  if (E.cx > rowlen) {
+    E.cx = rowlen;
   }
 }
 
